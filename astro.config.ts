cc8866a2f0177bd7cd3a18f1928bba163d47276e
zdragonite21 <dragonite_21@outlook.com>
2024-10-19 21:45:27 -0400

    new pages :+1:

--- astro.config.ts
+++ astro.config.ts
@@ -4,12 +4,8 @@ import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
   // used to generate images
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3000/',
+  site: "https://zacharyleong.github.io",
+  base: "zacharyleong.github.io",
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
