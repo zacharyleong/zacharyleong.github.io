f9a67311131299ffba4c2ffca779c79bebaaf5d1
zdragonite21 <zdragonite21@gmail.com>
2025-10-25 15:51:10 -0400

    mdx setup working!

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   // used to generate images
   site: "https://zacharyleong.github.io",
   trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  integrations: [sitemap(), UnoCSS({ injectReset: true }), mdx()],
   vite: {
     optimizeDeps: {
       exclude: ['@resvg/resvg-js'],
     },
   },
 });
+;
 ;
